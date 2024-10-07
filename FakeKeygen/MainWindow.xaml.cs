c396fa0eb5800044f32a1dc13878519159124538
minotto <minotto115@gmail.com>
2024-10-07 20:44:31 +0900

    first commit!!

--- FakeKeygen/MainWindow.xaml.cs
+++ FakeKeygen/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,32 @@ namespace FakeKeygen
         public MainWindow()
         {
             InitializeComponent();
+
+
+            var keyList = new List<string>()
+            {
+                "114", "514", "1919", "810", "114", "514", "1919", "810"
+            };
+            var shuffledList = keyList.OrderBy(a => Guid.NewGuid()).ToList();
+            string key = String.Join("-", shuffledList);
+            KeyTextBox.Text = key;
+
+        }
+
+        private void GenButton_Click(object sender, RoutedEventArgs e)
+        {
+            var keyList = new List<string>()
+            {
+                "114", "514", "1919", "810", "114", "514", "1919", "810"
+            };
+            var shuffledList = keyList.OrderBy(a => Guid.NewGuid()).ToList();
+            string key = String.Join("-", shuffledList);
+            KeyTextBox.Text = key;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
